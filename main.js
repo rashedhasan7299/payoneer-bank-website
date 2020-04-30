761cb32468ce4eda9e51f205eed668694ad92048
rashedhasan7299 <rashedhasan7299@gmail.com>
2020-04-30 15:48:14 +0600

    solved NAN and added alert for empty login, withdraw process

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
-// Display Trasaction area when login button is clicked
+// Display Transaction area when login button is clicked
 
 document.getElementById("btn-login").addEventListener("click", function() {
-    document.getElementById("transaction").style.display = "block";
-    document.getElementById("login").style.display = "none";
+
+    if(document.getElementById("email").value === '' || document.getElementById("password").value === '') {
+        alert("Enter all fields");
+    } else {
+        document.getElementById("transaction").style.display = "block";
+        document.getElementById("login").style.display = "none";
+    }
 });
 
 // Deposit Amount
 
 document.getElementById("btn-deposit").addEventListener("click", function() {
-    var deposit = parseFloat(document.getElementById("deposit").value);
-    var balance = parseFloat(document.getElementById("currentBalance").value);
-    console.log(balance);
-    document.getElementById("currentDeposit").innerText = deposit;
-    var totalBalance = balance + deposit;
+
+    var deposit = document.getElementById("deposit").value;
+    var depositAmount = parseFloat(deposit);
+    
+    var balance = document.getElementById("currentBalance").innerText;
+    var balanceAmount = parseFloat(balance);
+
+    var totalBalance = balanceAmount + depositAmount;
+
+    document.getElementById("currentDeposit").innerText = depositAmount;
     document.getElementById("currentBalance").innerText = totalBalance;
+    document.getElementById("deposit").value = '';
+});
+
+//Withdraw Amount
+
+document.getElementById("btn-withdraw").addEventListener("click", function() {
+
+    var withdraw = document.getElementById("withdraw").value;
+    var withdrawAmount = parseFloat(withdraw);
+    
+    var balance = document.getElementById("currentBalance").innerText;
+    var balanceAmount = parseFloat(balance);
+
+    var totalBalance = balanceAmount + -1 * withdrawAmount;
+
+    document.getElementById("currentWithdraw").innerText = withdrawAmount;
+    document.getElementById("currentBalance").innerText = totalBalance;
+    document.getElementById("withdraw").value = '';
 });
